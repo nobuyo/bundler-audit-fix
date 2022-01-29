082b3aa53153e1b3a5cff1896abaa2f7fc31c74d
nobuyo <longzechangsheng@gmail.com>
2022-01-29 13:04:03 +0900

    exit with non-zero code when bundle update has no effect for dependency reasons

--- lib/bundler/audit/fix/cli.rb
+++ lib/bundler/audit/fix/cli.rb
@@ -23,6 +23,7 @@ require 'bundler/cli'
 require 'bundler/cli/update'
 require 'bundler/audit/cli'
 require 'bundler/audit/database'
+require 'fileutils'
 
 module Bundler
   module Audit
@@ -75,7 +76,16 @@ module Bundler
             patcher = Patcher.new(dir, report, options[:gemfile_lock], options[:config])
             gems_to_update = patcher.patch
 
+            current_lockfile = StringIO.new(File.read(options[:gemfile_lock]))
             Bundler::CLI::Update.new({ gemfile: gemfile_path }, gems_to_update).run
+            updated_lockfile = StringIO.new(File.read(options[:gemfile_lock]))
+
+            if FileUtils.compare_stream(current_lockfile, updated_lockfile)
+              say 'All of the targets are staying in the same version for dependency reasons. Please resolve them manually.', :yellow
+              exit 1
+            end
+
+            exit 0
           rescue Bundler::GemfileNotFound, Bundler::GemfileLockNotFound => e
             say e.message, :red
             exit 1
